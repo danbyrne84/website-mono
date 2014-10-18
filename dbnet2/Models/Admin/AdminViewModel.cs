59d3a8628705b0c7113f0e96c5a70352e09f300b
Dan Byrne <dan@danielbyrne.net>
2014-10-18 14:42:33 +0100

    Updated git to latest version from server modifications

---
+++ dbnet2/Models/Admin/AdminViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace dbnet2.Models.Admin
+{
+    public class AdminViewModel
+    {
+        public VpnInformation VpnConnection { get; set; }
+
+        public AdminViewModel()
+        {
+            VpnConnection = new VpnInformation
+            {
+                Enabled = true,
+                Configuration = "EU AirVPN 443",
+                Received = 3242.1231,
+                Sent = 126.3212
+            };
+        }
+    }
+
+    public class VpnInformation
+    {
+        public bool Enabled { get; set; }
+        public string Configuration { get; set; }
+        public double Sent { get; set; }
+        public double Received { get; set; }
+    }
+}
